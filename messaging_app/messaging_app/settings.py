8e4c7fa0f3d14d66b4f5b927adc87dd7c66ae85e
Eilla-7 <eilafadel2238@gmail.com>
2024-12-31 07:39:21 +0400

    Setting up the REST Framework

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -40,6 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
